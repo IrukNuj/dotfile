28762d6084eb016c61babf7c64c8f7527ca06b99
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-08-06 15:11:54 +0900

    add: git config

--- setting.sh
+++ setting.sh
@@ -1,3 +1,5 @@
 cp ~/dotfile/.bash_profile ~/.bash_profile
 cp ~/dotfile/.bashrc ~/.bashrc     
 cp ~/dotfile/.netrc ~/.netrc    
+git config --global user.name "IrukNuj"
+git config --global user.email b1716568@planet.kanazawa-it.ac.jp
