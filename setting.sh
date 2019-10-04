d069414c8f6a1a839fa73605597edf32a4cf0a72
Junpei Kuriyama <b1716568@planet.kanazawa-it.ac.jp>
2019-10-05 03:39:22 +0900

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -3,3 +3,5 @@ cp ~/dotfile/.bashrc ~/.bashrc
 cp ~/dotfile/.netrc ~/.netrc    
 git config --global user.name "IrukNuj"
 git config --global user.email b1716568@planet.kanazawa-it.ac.jp
+source ~/.bashrc
+source ~/.bash_profile
